8875e647e7f0ade3931e03e4ae4f0b78018e1a4b
AJ <a.anjolaoluwa@yahoo.com>
2024-03-10 20:26:56 -0700

    Added Rental interface

---
+++ src/com/apartments/app/Rental.java
@@ -0,0 +1,19 @@
+package com.apartments.app;
+
+/**
+ * Lead Author(s):
+ * @author Anjolaoluwa Akinremi
+ * 
+ * References:
+ * Version/date: March-9-2024
+ * 
+ * Responsibilities of interface:
+ * Enforce a Rental object with certain properties
+ */
+
+public interface Rental
+{
+	double getRentAmount();
+	
+	void setRentAmount(double rent);
+}
